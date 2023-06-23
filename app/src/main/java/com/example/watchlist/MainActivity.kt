646263690d1ddc50806ad38eb10924788e610a26
jmeneses <woodstik92@gmail.com>
2023-06-22 19:17:30 -0500

    Apply Spotless to MainActivity

--- app/src/main/java/com/example/watchlist/MainActivity.kt
+++ app/src/main/java/com/example/watchlist/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.watchlist.ui.theme.WatchlistTheme
+import java.math.BigDecimal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +31,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
     )
 }
 

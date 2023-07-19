35766ae46512b95f908a437ca0553e1e171eaebd
jmeneses <woodstik92@gmail.com>
2023-07-19 12:15:22 -0500

    Rename MainActivity to NavigationActivity Setup NavHost

---
+++ app/src/main/java/com/example/watchlist/NavigationActivity.kt
@@ -0,0 +1,25 @@
+package com.example.watchlist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.watchlist.ui.theme.WatchlistTheme
+
+class NavigationActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            WatchlistTheme {
+                NavHost(
+                    navController = navController,
+                    startDestination = "test",
+                ) {
+                }
+            }
+        }
+    }
+}

1e500859377a84a32a73dac37d60fbc658804d76
jmeneses <woodstik92@gmail.com>
2023-07-25 15:42:37 -0500

    Add missing Hilt dependency for navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,6 +115,7 @@ dependencies {
 
     // Dependency Injection
     implementation("com.google.dagger:hilt-android:2.44.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     kapt("com.google.dagger:hilt-android-compiler:2.44.1")
 
     // Testing

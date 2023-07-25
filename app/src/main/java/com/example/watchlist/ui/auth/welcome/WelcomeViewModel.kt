4a5a401987eeec2ab5b55f53baccc19c00faac75
jmeneses <woodstik92@gmail.com>
2023-07-25 15:44:57 -0500

    Add hilt annotation to WelcomeViewModel

--- app/src/main/java/com/example/watchlist/ui/auth/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/example/watchlist/ui/auth/welcome/WelcomeViewModel.kt
@@ -3,8 +3,11 @@ package com.example.watchlist.ui.auth.welcome
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class WelcomeViewModel : ViewModel() {
+@HiltViewModel
+class WelcomeViewModel @Inject constructor() : ViewModel() {
 
     private val _state = MutableLiveData(WelcomeScreenState())
     val state: LiveData<WelcomeScreenState> = _state

2aa9ab08dbbe53dae944dce15474c860a1099bc3
jmeneses <woodstik92@gmail.com>
2023-07-31 15:10:07 -0500

    Create class for arguments and methods to receive changes in sign up form data

--- app/src/main/java/com/example/watchlist/ui/auth/signUp/SignUpNavigation.kt
+++ app/src/main/java/com/example/watchlist/ui/auth/signUp/SignUpNavigation.kt
@@ -2,6 +2,7 @@ package com.example.watchlist.ui.auth.signUp
 
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
@@ -11,6 +12,12 @@ import androidx.navigation.navArgument
 
 const val ROUTE_SIGN_UP = "sign_up"
 private const val ARG_EMAIL = "email"
+
+internal class SignUpArgs(val email: String) {
+    constructor(savedStateHandle: SavedStateHandle) :
+        this(checkNotNull(savedStateHandle[ARG_EMAIL]) as String)
+}
+
 fun NavGraphBuilder.signUpScreen() {
     composable(
         route = "$ROUTE_SIGN_UP/{$ARG_EMAIL}",
@@ -20,6 +27,8 @@ fun NavGraphBuilder.signUpScreen() {
         val screenState by viewModel.screenState.collectAsStateWithLifecycle()
         SignUpScreen(
             state = screenState,
+            onPasswordChange = { viewModel.onPasswordChange(it) },
+            onNameChange = { viewModel.onNameChange(it) },
         )
     }
 }
--- app/src/main/java/com/example/watchlist/ui/auth/signUp/SignUpViewModel.kt
+++ app/src/main/java/com/example/watchlist/ui/auth/signUp/SignUpViewModel.kt
@@ -1,14 +1,28 @@
 package com.example.watchlist.ui.auth.signUp
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class SignUpViewModel @Inject constructor() : ViewModel() {
+class SignUpViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+) : ViewModel() {
 
-    private val _screenState = MutableStateFlow(SignUpScreenState())
+    private val args = SignUpArgs(savedStateHandle)
+
+    private val _screenState = MutableStateFlow(SignUpScreenState(email = args.email))
     val screenState = _screenState.asStateFlow()
+
+    fun onPasswordChange(password: String) {
+        _screenState.update { it.copy(password = password) }
+    }
+
+    fun onNameChange(name: String) {
+        _screenState.update { it.copy(name = name) }
+    }
 }

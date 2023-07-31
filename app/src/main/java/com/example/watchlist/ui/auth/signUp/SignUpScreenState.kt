372612aff5faa4c8a05a30debe4a8b2eedaa9c34
jmeneses <woodstik92@gmail.com>
2023-07-31 15:23:50 -0500

    Update SignUpScreenState with submitState

--- app/src/main/java/com/example/watchlist/ui/auth/signUp/SignUpScreenState.kt
+++ app/src/main/java/com/example/watchlist/ui/auth/signUp/SignUpScreenState.kt
@@ -1,11 +1,22 @@
 package com.example.watchlist.ui.auth.signUp
 
+import com.example.watchlist.data.model.SubmitState
+import com.example.watchlist.data.response.SignUpResponse
+
 data class SignUpScreenState(
     val email: String = "",
     val name: String = "",
     val password: String = "",
+    val submitState: SubmitState<SignUpResponse> = SubmitState.Idle,
 ) {
 
     val enableSubmit: Boolean
-        get() = name.isNotEmpty() && password.isNotEmpty()
+        get() = name.isNotEmpty() && password.isNotEmpty() &&
+            submitState !is SubmitState.InProgress
+
+    val nameReadOnly: Boolean
+        get() = submitState !is SubmitState.InProgress
+
+    val passwordReadOnly: Boolean
+        get() = submitState !is SubmitState.InProgress
 }
